6a088ce76876fa83b8d3b93fbba84152748b4d21
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-12-19 14:48:01 -0300

    FIX: Inserindo then e catch no migrations

--- src/data/migrations.ts
+++ src/data/migrations.ts
@@ -1,7 +1,10 @@
 import { connection } from "./connection"
 
-const createTables = async () =>{
-    await connection.raw( 
+const erroMensagem = (error:any) => {console.log(error.message ||  error.sqlMessage);
+
+}
+const createTables = () => connection
+    .raw(
         `
         CREATE TABLE IF NOT EXISTS Estudante_System (
             id VARCHAR(255) PRIMARY KEY
@@ -15,18 +18,18 @@ const createTables = async () =>{
             id VARCHAR(255) PRIMARY KEY
             estudante_id VARCHAR(255) FOREIGN KEY NOT NULL
             hobby_id VARCHAR(255) FOREIGN KEY NOT NULL
-        )
+        );
 
         CREATE TABLE IF NOT EXISTS Hobby(
             id VARCHAR(255) PRIMARY KEY
             nome VARCHAR(255) NOT NULL UNIQUE
-        )
+        );
 
         CREATE TABLE IF NOT EXISTS Turma(
             id VARCHAR(255) PRIMARY KEY
             nome VARCHAR(255)
             modulo: VARCHAR(255) DEFAULT 0
-        )
+        );
 
         CREATE TABLE IF NOT EXISTS Docente_System (
             id VARCHAR(255) PRIMARY KEY
@@ -40,13 +43,14 @@ const createTables = async () =>{
             id VARCHAR(255) PRIMARY KEY
             docente_id VARCHAR(255) FOREIGN KEY NOT NULL
             especialidade_id VARCHAR(255) FOREIGN KEY NOT NULL
-        )
+        );
 
         CREATE TABLE IF NOT EXISTS Especialidade( 
             id VARCHAR(255) PRIMARY KEY
             nome VARCHAR(255) NOT NULL UNIQUE
-        )
-
+        );
         `
-    )
-}
+    ).then(() => {console.log("Tabela criada com sucesso!")})
+    .catch(erroMensagem)
+    
+    createTables()
