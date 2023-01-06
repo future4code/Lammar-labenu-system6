fafa859d5813bd1379c1320d06fca088def43af4
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2023-01-06 19:07:02 -0300

    FIX: Inserindo o id do estudante na resposta da criação para que ele possa inserir os hobbies

--- src/endpoints/Estudantes/criarEstudante.ts
+++ src/endpoints/Estudantes/criarEstudante.ts
@@ -9,9 +9,16 @@ export default async function criarEstudante(
     try{
         const {nome, email, data_nasc} = req.body
         const turma = req.body.turma_id
+        const id = Math.random()
 
-        if(!nome || !email || !data_nasc || !turma){
-            throw new Error ("Insira todos os parâmetros necessários: Nome, email, data de nascimento e turma.")
+        if(!nome){
+            return res.status(422).send("Insira o nome do estudante.")
+        }else if(!email){
+            return res.status(422).send("Insira o email do estudante.")
+        }else if(!data_nasc){
+            return res.status(422).send("Insira a data de nascimento do estudante no formato AAAA-MM-DD")
+        }else if (!turma){
+            return res.status(422).send("Insira o id da turma")
         }
 
         let resultadoTurma = await connection.raw( 
@@ -26,7 +33,7 @@ export default async function criarEstudante(
 
         await connection.insert(
             [{
-                id: Math.random(),
+                id,
                 nome: nome,
                 email: email,
                 data_nasc: data_nasc,
@@ -34,7 +41,7 @@ export default async function criarEstudante(
             }]
         ).into("Estudante")
 
-        res.status(201).send("Estudante criado com sucesso! Próxima etapa: Inserir seus hobbys")
+        res.status(201).send(`Estudante criado com sucesso! Próxima etapa: Inserir seus hobbys, esse é o seu id ${id}`)
     }catch(error:any){
         console.log(error)
     }
--- src/endpoints/Turmas/listarTurmas.ts
+++ src/endpoints/Turmas/listarTurmas.ts
@@ -4,7 +4,7 @@ import { connection } from "../../data/connection";
 export default async function listarTurmas(req:Request, res:Response) {
     try{
         let resultado = await connection.raw( 
-            `SELECT * FROM Turma WHERE modulo between 1 and 6`
+            `SELECT * FROM Turma`
         )
         res.status(200).send(resultado[0])
     }catch(error:any){
